d378a217b23ed0182fb21d7a68f78eaa00adfebf
Dragon <dragon.program.sib@gmail.com>
2022-02-01 21:37:07 +0700

    The contents of the "App.vue" file have been changed.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+	<div class="app">
+		<h1>Страница с постами</h1>
+		<my-button @click="showDialog" style="margin: 15px 0">Создать пост</my-button>
+		<my-dialog v-model:show="dialogVisible">
+			<post-form @create="createPost" />
+		</my-dialog>
+		<!-- <post-form @create="createPost" /> -->
+		<post-list :posts="posts" @remove="removePost" />
+	</div>
 </template>
 
+<script>
+	import PostForm from "@/components/PostForm";
+	import PostList from "@/components/PostList";
+	export default {
+		components: {
+			PostForm,
+			PostList,
+		},
+		data() {
+			return {
+				posts: [
+					{ id: 1, title: "Vue3 (a)", body: "Описание поста - 1" },
+					{ id: 2, title: "Vue3 (b)", body: "Описание поста - 2" },
+					{ id: 3, title: "Vue3 (c)", body: "Описание поста - 3" },
+				],
+				dialogVisible: false,
+			};
+		},
+		methods: {
+			createPost(post) {
+				this.posts.push(post);
+				this.dialogVisible = false;
+			},
+			removePost(post) {
+				this.posts = this.posts.filter((p) => p.id !== post.id);
+			},
+			showDialog() {
+				this.dialogVisible = true;
+			},
+		},
+	};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+	.app {
+		padding: 20px;
+	}
 </style>
