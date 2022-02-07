6cf2d5471e7695bd466d8d284f2eb60dd95a905c
Dragon <dragon.program.sib@gmail.com>
2022-02-08 01:13:50 +0700

    The output of pagination is visualized, including the selection of the current page.

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,18 @@
 			v-if="!isPostsLoading"
 		/>
 		<div v-else>Идёт загрузка...</div>
+		<div class="page__wrapper">
+			<div
+				v-for="pageNumber in totalPages"
+				:key="pageNumber"
+				class="page"
+				:class="{
+					'page-current': page === pageNumber,
+				}"
+			>
+				{{ pageNumber }}
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -38,13 +50,16 @@
 			return {
 				posts: [
 					/*{ id: 1, title: "Vue3 (a)", body: "Описание поста - 1" },
-									{ id: 2, title: "Vue3 (b)", body: "Описание поста - 2" },
-									{ id: 3, title: "Vue3 (c)", body: "Описание поста - 3" },*/
+																	{ id: 2, title: "Vue3 (b)", body: "Описание поста - 2" },
+																	{ id: 3, title: "Vue3 (c)", body: "Описание поста - 3" },*/
 				],
 				dialogVisible: false,
 				isPostsLoading: false,
 				selectedSort: "",
 				searchQuery: "",
+				page: 1,
+				limit: 10,
+				totalPages: 0,
 				sortOptions: [
 					{ value: "title", name: "По названию" },
 					{ value: "body", name: "По содержимому" },
@@ -66,7 +81,16 @@
 				try {
 					this.isPostsLoading = true;
 					const response = await axios.get(
-						"https://jsonplaceholder.typicode.com/posts?_limit=10"
+						"https://jsonplaceholder.typicode.com/posts",
+						{
+							params: {
+								_page: this.page,
+								_limit: this.limit,
+							},
+						}
+					);
+					this.totalPages = Math.ceil(
+						response.headers['x-total-count'] / this.limit
 					);
 					this.posts = response.data;
 					// console.log(response);
@@ -82,11 +106,19 @@
 		},
 		computed: {
 			sortedPosts() {
-				return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]));
+				return [...this.posts].sort((post1, post2) =>
+					post1[this.selectedSort]?.localeCompare(
+						post2[this.selectedSort]
+					)
+				);
 			},
 			sortedAndSearchedposts() {
-				return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
-			}
+				return this.sortedPosts.filter((post) =>
+					post.title
+						.toLowerCase()
+						.includes(this.searchQuery.toLowerCase())
+				);
+			},
 		},
 		watch: {
 			dialogVisible(newValue) {
@@ -111,5 +143,19 @@
 		display: flex;
 		justify-content: space-between;
 		margin: 15px 0;
+	}
+
+	.page {
+		border: 1px solid black;
+		padding: 10px;
+	}
+
+	.page-current {
+		border: 2px solid teal;
+	}
+
+	.page__wrapper {
+		display: flex;
+		margin-top: 15px;
 	}
 </style>
